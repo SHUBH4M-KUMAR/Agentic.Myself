d7d206664370d94620ee3876a3ccd9323a9a447d
Shubham Kumar <37694707+SHUBH4M-KUMAR@users.noreply.github.com>
2025-06-17 15:02:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ from elevenlabs import ElevenLabs, play
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # or "*" for all
+    allow_origins=["*"],  # or "*" for all
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
